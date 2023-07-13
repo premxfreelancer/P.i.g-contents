0f8a98066eb930439194f606b6e5ff0cec6026e2
Premopfffff <127723063+Premopfffff@users.noreply.github.com>
2023-07-13 14:00:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+  // Hide the loading screen
+  var loadingScreen = document.querySelector('.loading-screen');
+  loadingScreen.style.display = 'none';
+
+  // Show the content area
+  var content = document.querySelector('.content');
+  content.style.display = 'block';
+});
